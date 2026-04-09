09028c0755d36e4c147df0c2ffcecf4fafada5c8
VinCBR900 <v.p.crabtree@gmail.com>
2026-04-09 15:56:43 +0100

    Report correct BASIC line number in runtime errors

--- basic.asm
+++ basic.asm
@@ -155,6 +155,7 @@ progloc:    equ 0x8000  ; Program address
                         ; (notice optimizations dependent on this address)
 
 stack:      equ 0xff00  ; Stack address
+current_line:   equ vars-2 ; Current BASIC line during RUN
 max_line:   equ 1000    ; First unavailable line number
 max_length: equ 20      ; Maximum length of line inc CR
 max_size:   equ max_line*max_length ; Max. program size
@@ -236,16 +237,24 @@ f4:     call get_variable       ; Try variable
         ; BUG: Cant get current BASIC line number
         ;
 error:
-	push ax
-	;mov si,error_message
-        ;call print_2    ; Show error message
         call new_line
         mov al,'@'
         call output
-        pop ax
+        cmp sp,stack-2      ; Interactive mode has no current line
+        je f33
+        mov ax,[current_line]
+        jmp f34
+f33:    xor ax,ax
+f34:
         call output_number
         call new_line
         jmp main_loop   ; Exit to main loop
+
+        ;
+        ; Handle 'rem' statement (comment)
+        ;
+rem_statement:
+        ret
 
         ;
         ; Handle 'list' statement
@@ -416,7 +425,7 @@ f26:
         call f26            ; Yes, output left side
 f8:     pop ax
         add al,'0'          ; Output remainder as...
-        jmp short output    ; ...ASCII digit
+        jmp output          ; ...ASCII digit
 
         ;
         ; Read number in input.
@@ -464,6 +473,13 @@ f27:    cmp sp,stack-2      ; In interactive mode?
         mov [stack-4],ax    ; No, replace the saved address of next line
         ret
 f31:
+        push ax
+        sub ax,program       ; Convert pointer into BASIC line number
+        xor dx,dx
+        mov cx,max_length
+        div cx
+        mov [current_line],ax
+        pop ax
         push ax
         pop si
         add ax,max_length   ; Point to next line
@@ -576,6 +592,9 @@ statements:
 
         db 5,"list"
         dw list_statement
+
+        db 4,"rem"
+        dw rem_statement
 
         db 4,"run"
         dw run_statement
